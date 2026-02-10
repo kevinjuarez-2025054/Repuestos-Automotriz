8905a4076ab44987399a497a8022fcdf352b7afe
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-10 08:40:10 -0600

    feat:Se ha creado los atributos de la entidad Proveedores en la entity Proveedores

---
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/entity/Proveedores.java
@@ -0,0 +1,67 @@
+package com.kevinlarios.RepuestosAutomotriz.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "Proveedores")
+public class Proveedores {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer idProveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
+
+    @Column(name = "telefono_proveedor")
+    private Integer telefonoProveedor;
+
+    @Column(name = "direccion")
+    private String direccionProveedor;
+
+    @Column(name = "email_proveedor")
+    private String emailProveedor;
+
+
+    //Getters and Setters
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public Integer getTelefonoProveedor() {
+        return telefonoProveedor;
+    }
+
+    public void setTelefonoProveedor(Integer telefonoProveedor) {
+        this.telefonoProveedor = telefonoProveedor;
+    }
+
+    public String getDireccionProveedor() {
+        return direccionProveedor;
+    }
+
+    public void setDireccionProveedor(String direccionProveedor) {
+        this.direccionProveedor = direccionProveedor;
+    }
+
+    public String getEmailProveedor() {
+        return emailProveedor;
+    }
+
+    public void setEmailProveedor(String emailProveedor) {
+        this.emailProveedor = emailProveedor;
+    }
+}
