cbb6527358511be7e1748d121534b0cbd7e3a04a
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-10 09:06:26 -0600

    feat:Se subio ProveedoresService con los metodos a emplear

---
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/service/ProveedoresService.java
@@ -0,0 +1,13 @@
+package com.kevinlarios.RepuestosAutomotriz.service;
+
+import com.kevinlarios.RepuestosAutomotriz.entity.Proveedores;
+
+import java.util.List;
+
+public interface ProveedoresService {
+    List<Proveedores> getAllProveedor();
+    Proveedores getProveedoresById(Integer id);
+    Proveedores saveProvedores(Proveedores proveedores);
+    Proveedores updateProveedores(Integer id,Proveedores proveedores);
+    void deleteProveedores(Integer id);
+}
