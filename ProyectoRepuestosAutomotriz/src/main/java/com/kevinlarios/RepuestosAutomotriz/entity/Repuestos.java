6c4515e58e7d6b4a9c6b1f9b059abe162afe4eff
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-11 10:17:26 -0600

    faet:Se ha completado la entity Repuestos

--- ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/entity/Repuestos.java
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/entity/Repuestos.java
@@ -12,4 +12,69 @@ public class Repuestos {
 
     @Column(name = "nombre_repuesto")
     private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private boolean precioCompra;
+
+    @Column(name = "precio_venta")
+    private boolean precioVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_proveedor")
+    private Proveedores proveedores;
+
+
+    // Getters and Setters //
+
+
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+
+    public String getNombreRepuesto() {
+        return nombreRepuesto;
+    }
+
+    public void setNombreRepuesto(String nombreRepuesto) {
+        this.nombreRepuesto = nombreRepuesto;
+    }
+
+    public String getCategoriaRepuesto() {
+        return categoriaRepuesto;
+    }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) {
+        this.categoriaRepuesto = categoriaRepuesto;
+    }
+
+    public boolean isPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(boolean precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public boolean isPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(boolean precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Proveedores getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(Proveedores proveedores) {
+        this.proveedores = proveedores;
+    }
 }
