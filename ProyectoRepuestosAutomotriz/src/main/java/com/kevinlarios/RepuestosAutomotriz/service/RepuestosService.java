30d399ca7a4b2a519bb2a1bf7dbbf2be434de8e5
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-11 10:46:45 -0600

    feat:Se ha subido RepuestosService

--- ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/service/RepuestosService.java
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/service/RepuestosService.java
@@ -1,4 +1,13 @@
 package com.kevinlarios.RepuestosAutomotriz.service;
 
+import com.kevinlarios.RepuestosAutomotriz.entity.Repuestos;
+
+import java.util.List;
+
 public interface RepuestosService {
+    List<Repuestos> getAllRepuestos();
+    Repuestos getRepuestosById(Integer id);
+    Repuestos saveRepuestos(Repuestos repuestos) throws RuntimeException;
+    Repuestos updateRepuestos(Integer id,Repuestos repuestos);
+    void deleteRepuestos(Integer id);
 }
