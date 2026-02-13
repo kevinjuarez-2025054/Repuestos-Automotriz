ab2c13ab55e79f49cf8651709da81a6fe4bcac6a
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-12 20:34:55 -0600

    feat:Se hafregaron las anotaciones

--- ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/entity/Repuestos.java
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/entity/Repuestos.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "Repuestos")
 public class Repuestos {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_repuesto")
     private Integer idRepuesto;
 
