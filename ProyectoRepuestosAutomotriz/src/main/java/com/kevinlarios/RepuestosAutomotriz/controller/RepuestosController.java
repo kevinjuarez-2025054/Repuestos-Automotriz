6f157432b124eba680d34468e14947f92094ef02
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-02-12 18:35:02 -0600

    feat:Se subio RepuestosController

---
+++ ProyectoRepuestosAutomotriz/src/main/java/com/kevinlarios/RepuestosAutomotriz/controller/RepuestosController.java
@@ -0,0 +1,51 @@
+package com.kevinlarios.RepuestosAutomotriz.controller;
+
+import com.kevinlarios.RepuestosAutomotriz.entity.Repuestos;
+import com.kevinlarios.RepuestosAutomotriz.service.RepuestosService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/repuestos")
+public class RepuestosController {
+    public final RepuestosService repuestosService;
+
+    public RepuestosController(RepuestosService repuestosService) {
+        this.repuestosService = repuestosService;
+    }
+    @GetMapping
+    public List<Repuestos> getAllRepuestos(){return repuestosService.getAllRepuestos();}
+
+    @PostMapping
+    public ResponseEntity<Object> createRepuestos(Repuestos repuestos){
+        try {
+            Repuestos createRepuesto = repuestosService.saveRepuestos(repuestos);
+            return new ResponseEntity<>(createRepuesto, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping
+    public  ResponseEntity<Object> updateRepuestos(Integer id,Repuestos repuestos){
+        try {
+            Repuestos updateRepuesto = repuestosService.updateRepuestos(id,repuestos);
+            return new ResponseEntity<>(updateRepuesto,HttpStatus.OK);
+        }catch (RuntimeException e){
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deleteRepuestos(Integer id){
+        try {
+            repuestosService.deleteRepuestos(id);
+            return ResponseEntity.ok("Repuesto eliminado correctamente");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
